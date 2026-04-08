88eb49e84913d14693e2b6ba28240be94c195e99
andyrueda439 <ajrueda@uamv.edu.ni>
2026-04-07 19:49:06 -0500

    Se creo la Clase Cuenta

--- src/caso1_banco/models/Cuenta.java
+++ src/caso1_banco/models/Cuenta.java
@@ -1,4 +1,24 @@
 package caso1_banco.models;
 
 public class Cuenta {
+    private String numeroCuenta;
+    private double saldo;
+
+
+    public Cuenta(String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "numeroCuenta='" + numeroCuenta + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
